8d07c1d5d276a6f187bf41e309130f183d0e7664
Gabriel <gabriel.agudelo@utp.edu.co>
2022-01-15 19:50:53 -0500

    end

--- Sistema.js
+++ Sistema.js
@@ -32,3 +32,46 @@ class Producto {
         return `idProducto: ${this._idProducto}, nombre: ${this._nombre}, precio: ${this._precio}`;
     }
 }
+
+class Orden {
+
+    static contadorOrdenes = 0;
+    static get MAX_PRODUCTOS(){
+        return 5;
+    }
+
+    constructor (){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden() {
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if (this._productos.length  < Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+        }
+        else {
+            console.log("no se pueden agregar mas productos");
+        }
+    }
+
+    calcularTotal(){
+        let totalVenta = 0;
+        for (let producto of this._productos){
+            totalVenta += producto.precio;
+        }
+    }
+
+    mostrarOrden(){
+        let productosOrden = ' ';
+        for (let producto of this._productos){
+            productosOrden += producto.toString() + ' ';
+        }
+
+        console.log(`orden: ${this._idOrden} total: ${this.calcularTotal()} productos: ${productosOrden}`);
+    }
+}
